2faef8b4971a315d23a1ebd9f9bd1967c113a50a
Erulinman <erulinman.work@protonmail.com>
2022-01-07 12:09:17 +0300

    Implement navigation

--- app/src/main/java/info/erulinman/mangadexreader/MainActivity.kt
+++ app/src/main/java/info/erulinman/mangadexreader/MainActivity.kt
@@ -5,19 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.fragment.app.commit
-import info.erulinman.mangadexreader.ui.fragments.MangaListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                add(R.id.mainFragmentContainer, MangaListFragment())
-            }
-        }
     }
 
     fun closeKeyboard(view: View) {
